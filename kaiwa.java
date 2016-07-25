c953ef3a6bd2e307e537be9247994ab3658fdef3
nakagawa0806 <nakagawa@s15.yeskist.net>
2016-07-25 10:53:05 +0900

    追加

--- kaiwa.java
+++ kaiwa.java
@@ -16,30 +16,45 @@ public class kaiwa {
 		 
 		
 	switch (n) {
-	case 1:System.out.println("1"); break;
-	case 2:System.out.println("2"); break;
-	case 3:System.out.println("3"); break;
-	case 4:System.out.println("4"); break;
+	case 1:System.out.println("1:いつもどおり"); break;
+	case 2:System.out.println("2:絶好調"); break;
+	case 3:System.out.println("3:体調が悪い"); break;
+	case 4:System.out.println("4:チョベリグです。"); break;
 	}
 	if(n == 1) {
-		System.out.println("[]");
-		System.out.println("1:");
-		System.out.println("2:"); 
-		System.out.println("3:");
+		System.out.println("[何もなかったんですか?]");
+		System.out.println("1:何にもない");
+		System.out.println("2:そういえばいい事があった"); 
+		System.out.println("3:そういえば悪い事があった");
+		System.out.println("4:そういえば悲しい事があった");
 		System.out.print("選択："); int x = stdIn.nextInt();
 	}
 		switch (x) {
-	case 1:System.out.println("1"); break;
-	case 2:System.out.println("2"); break;
-	case 3:System.out.println("3"); break;
-	case 4:System.out.println("4"); break;
+	case 1:System.out.println("1:何にもない"); break;
+	case 2:System.out.println("2:そういえばいい事があった"); break;
+	case 3:System.out.println("3:そういえば悪い事があった"); break;
+	case 4:System.out.println("4:そういえば悲しい事があった"); break;
+	}
+	if(x == 1) {
+		System.out.println("[なかったんですか?]");
+		System.out.println("1:あ、そういえば");
+		System.out.println("2:そちらはなんかありました"); 
+		System.out.println("3:明日はありますかね？");
+		System.out.println("4:暇でした");
+		System.out.print("選択:"); int a1 = stdIn.nextInt();
+	}
+		switch (a1) {
+	case 1:System.out.println("1:あ、そういえば"); break;
+	case 2:System.out.println("2:そちらはなんかありました？"); break;
+	case 3:System.out.println("3:明日はありますかね？"); break;
+	case 4:System.out.println("4:暇でした"); break;
 	}
 	}else if(n == 2) {
 		System.out.println("[]");
 		System.out.println("1:");
 		System.out.println("2:"); 
 		System.out.println("3:");
-		System.out.print("選択："); int x = stdIn.nextInt();
+		System.out.print("選択"); int x = stdIn.nextInt();
 	}
 		switch (x) {
 	case 1:System.out.println("1"); break;
@@ -52,7 +67,7 @@ public class kaiwa {
 		System.out.println("1:");
 		System.out.println("2:"); 
 		System.out.println("3:");
-		System.out.print("選択："); int x = stdIn.nextInt();
+		System.out.print("選択:"); int x = stdIn.nextInt();
 	}
 		switch (x) {
 	case 1:System.out.println("1"); break;
@@ -66,7 +81,7 @@ public class kaiwa {
 		System.out.println("1:");
 		System.out.println("2:"); 
 		System.out.println("3:");
-		System.out.print("選択："); int x = stdIn.nextInt();
+		System.out.print("選択:"); int x = stdIn.nextInt();
 	}
 		switch (x) {
 	case 1:System.out.println("1"); break;
@@ -74,6 +89,5 @@ public class kaiwa {
 	case 3:System.out.println("3"); break;
 	case 4:System.out.println("4"); break;
 	}
-	
 }
 }
